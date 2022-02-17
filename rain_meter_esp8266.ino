9fa461023b17b55ca32f6a9a1cfd017bbb17b440
Jiraphat (Saifah) <jiraphat.t@touchtechnologies.co.th>
2022-02-17 10:47:03 +0700

    add

--- rain_meter_esp8266.ino
+++ rain_meter_esp8266.ino
@@ -1,23 +1,16 @@
 //https://dl.espressif.com/dl/package_esp32_index.json
 //http://arduino.esp8266.com/stable/package_esp8266com_index.json
+// 2.794 millimeter per once
+// 90 miilmeter limit
+// 20 min cooldwon reset
+void ICACHE_RAM_ATTR countWater();
 
-
-/**
- * Created by K. Suwatchai (Mobizt)
- * 
- * Email: k_suwatchai@hotmail.com
- * 
- * Github: https://github.com/mobizt
- * 
- * Copyright (c) 2021 mobizt
- *
-*/
-
-#if defined(ESP32)
-#include <WiFi.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#endif
+//#if defined(ESP32)
+//#include <WiFi.h>
+//#elif defined(ESP8266)
+//#include <ESP8266WiFi.h>
+//#endif
+#include <AutoConnect.h>
 #include <Firebase_ESP_Client.h>
 
 //Provide the token generation process info.
@@ -26,10 +19,13 @@
 //Provide the RTDB payload printing info and other helper functions.
 #include <addons/RTDBHelper.h>
 
-/* 1. Define the WiFi credentials */
-#define WIFI_SSID "KanomPang"
-#define WIFI_PASSWORD "10111409"
+#include <TridentTD_LineNotify.h>
+#include <SPI.h>
+#include <SD.h>
 
+/* 1. Define the WiFi credentials */
+//#define WIFI_SSID "babe"
+//#define WIFI_PASSWORD "babe2509"
 //For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 
 /* 2. Define the API Key */
@@ -42,35 +38,118 @@
 #define USER_EMAIL "cpe.rainmeter@gmail.com"
 #define USER_PASSWORD "cpe123456"
 
+#define LINE_TOKEN  "f0E3HB6o7EubJvIVNw8AUC8QpoOeJn7H8sD3mUYm5K1"
+
+//  Rtc DS3231
+#include <Wire.h>
+#include <RtcDS3231.h>
+RtcDS3231<TwoWire> Rtc(Wire);
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
 //Define Firebase Data object
 FirebaseData fbdo;
-
 FirebaseAuth auth;
 FirebaseConfig config;
 
-unsigned long sendDataPrevMillis = 0;
+const char* ssid = "Rain Meter (Setting)";
+const char* password = "11111111";
 
-unsigned long count = 0;
+//boolean pordMode = true;
+boolean pordMode = false;
 
-void setup()
-{
+AutoConnect      Portal;
+AutoConnectConfig Config(ssid, password);
 
+unsigned long currentMillis, previousMillis1, previousMillis2, previousMillis3, previousMillis4, previousMillis5, previousMillis6, previousMillis7 = 0;
+// Task 1 : Push data
+// Task 2 : Delete Data 7 day ago
+// Task 3 : Handle WiFi
+// Task 4 : Debounce Interrupt
+// Task 5 :
+// Task 6 : Reset cooldwon water
+// Task 7 : Debounce Line notify
+int t3Interval = 2000, t4Interval = 200, t5Interval = 25000;
+
+int t1Interval = pordMode ? 60000 * 15 : 15000;
+int t2Interval = pordMode ? 60000 * 60 : 10000;
+int t6Interval = pordMode ? 60000 : 1000;
+int t7Interval = pordMode ? 60000 * 20 : 20000;
+
+char dateTimestring[20];
+char datestring[11];
+String dateTimeNow = "";
+
+uint8_t countWaterPin = D3;
+volatile int waterCount = 0;
+bool counted = false;
+const float amountOfWater = 2.794;
+const int waterLimit = 90;
+
+const int cooldownReset = 20;
+int previousWaterCount = 0;
+int duplicateCount = 0;
+boolean sendLineNotify = true;
+
+int day = 0;
+int month = 0;
+int year = 0;
+
+File indexFile;
+File dataFile;
+const int sdCardPin = D4;
+
+void setup() {
+  //  ปี เดือน วัน ชั่วโมง นาที วินาที
+  //  สำหรับตั้งเวลา
+  //  RtcDateTime compiled = RtcDateTime(2021, 12, 11, 17, 19, 00);
+  //  Rtc.SetDateTime(compiled);
+  delay(1000);
   Serial.begin(115200);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
+  pinMode(countWaterPin, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(countWaterPin), countWater, FALLING);
+
+  if (!SD.begin(sdCardPin)) {
+    Serial.println("initialization SD Card failed !");
+    return;
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-  Serial.println("55555");
-  Serial.println("55555");
-  Serial.println("55555");
+
+  Rtc.Begin();
+  RtcDateTime now = Rtc.GetDateTime();
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+
+  // กำหนด Line Token
+  LINE.setToken(LINE_TOKEN);
+
+  Serial.println("Starting.....  X)");
+  Config.autoReconnect = true;    // Attempt automatic reconnection.
+  Config.reconnectInterval = 1;   // Seek interval time is 180[s].
+  digitalWrite(LED_BUILTIN, LOW);
+  //  Config.ticker = true;
+  //  Config.tickerPort = 2;
+  //  Config.tickerOn = LOW;
+  Portal.config(Config);
+  AutoConnectConfig(ssid, password);
+  if (Portal.begin()) {
+    Serial.println("WiFi connected: " + WiFi.localIP().toString());
+    digitalWrite(LED_BUILTIN, HIGH);
+    LINE.notifySticker("เครื่องวัดปริมาณน้ำฝนออนไลน์แล้ว !", 446, 1993);
+    //    Config.ticker = false;
+  }
+
+  //  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  //  Serial.print("Connecting to Wi-Fi");
+  //  while (WiFi.status() != WL_CONNECTED)
+  //  {
+  //    Serial.print(".");
+  //    delay(300);
+  //  }
+  //  Serial.println();
+  //  Serial.print("Connected with IP: ");
+  //  Serial.println(WiFi.localIP());
+  //  Serial.println();
 
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
@@ -99,152 +178,201 @@ void setup()
   Firebase.begin(&config, &auth);
 
   //Comment or pass false value when WiFi reconnection will control by your code or third party library
-  Firebase.reconnectWiFi(true);
-
+  Firebase.reconnectWiFi(false);
   Firebase.setDoubleDigits(5);
+}
 
-  /** Timeout options.
+void t1Callback() {
+  RtcDateTime now = Rtc.GetDateTime();
+  //  Serial.println(now);
+  //  Serial.printf("Set bool... %s\n", Firebase.RTDB.setBool(&fbdo, "/test/bool", count % 2 == 0) ? "ok" : fbdo.errorReason().c_str());
+  //  Serial.printf("Get bool... %s\n", Firebase.RTDB.getBool(&fbdo, "/test/bool") ? fbdo.to<bool>() ? "true" : "false" : fbdo.errorReason().c_str());
+  //  bool bVal;
+  //  Serial.printf("Get bool ref... %s\n", Firebase.RTDB.getBool(&fbdo, "/test/bool", &bVal) ? bVal ? "true" : "false" : fbdo.errorReason().c_str());
+  //  Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/test/int", count) ? "ok" : fbdo.errorReason().c_str());
+  //  Serial.printf("Get int... %s\n", Firebase.RTDB.getInt(&fbdo, "/test/int") ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
+  FirebaseJson json;
+  //  if (count == 0)
+  //  {
 
-  //WiFi reconnect timeout (interval) in ms (10 sec - 5 min) when WiFi disconnected.
-  config.timeout.wifiReconnect = 10 * 1000;
+  dateTimeNow = printDateTime(now);
+  json.set("water", waterCount * amountOfWater);
+  json.set("time", dateTimeNow);
+  //  json.set("time/.sv", "timestamp");
 
-  //Socket connection and SSL handshake timeout in ms (1 sec - 1 min).
-  config.timeout.socketConnection = 10 * 1000;
+  //  Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, "/testset", &json) ? "ok" : fbdo.errorReason().c_str());
+  //  Firebase.RTDB.set(&fbdo, "/testset", &json);
+  Firebase.RTDB.set(&fbdo, "/rainmeter/" + printDate(now) + "/" + dateTimeNow, &json);
+  //  Firebase.RTDB.pushJSON(&fbdo, "/" + printDate(now), &json);
+  //  Serial.println(Firebase.RTDB.pushJSON(&fbdo, "/" + printDateTime(now), &json));
+  //  Serial.printf("Push data with timestamp... %s\n", Firebase.RTDB.pushJSON(&fbdo, "/" + printDateTime(now), &json) ? "ok" : fbdo.errorReason().c_str());
 
-  //Server response read timeout in ms (1 sec - 1 min).
-  config.timeout.serverResponse = 10 * 1000;
+  //  Firebase.RTDB.deleteNode(&fbdo, "/test/append");
 
-  //RTDB Stream keep-alive timeout in ms (20 sec - 2 min) when no server's keep-alive event data received.
-  config.timeout.rtdbKeepAlive = 45 * 1000;
 
-  //RTDB Stream reconnect timeout (interval) in ms (1 sec - 1 min) when RTDB Stream closed and want to resume.
-  config.timeout.rtdbStreamReconnect = 1 * 1000;
+  //  }
+  //  else
+  //  {
+  //  json.add(String(count), "smart!");
+  //  Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, "/test/json/value/round", &json) ? "ok" : fbdo.errorReason().c_str());
+  //  }
+  if (waterCount * amountOfWater >= waterLimit && sendLineNotify) {
+    //        LINE.notify("ปริมาณน้ำฝนเกิน 90 มิลลิเมตรแล้ว !");
+    LINE.notifySticker("ปริมาณน้ำฝนเกิน 90 มิลลิเมตรแล้ว !", 11538, 51626522);
+    sendLineNotify = false;
+  }
+  Serial.println();
+}
 
-  //RTDB Stream error notification timeout (interval) in ms (3 sec - 30 sec). It determines how often the readStream
-  //will return false (error) when it called repeatedly in loop.
-  config.timeout.rtdbStreamError = 3 * 1000;
+void t2Callback() {
+  RtcDateTime now = Rtc.GetDateTime();
+  //  int tmpDay = 1;
+  //  int tmpMonth = 1;
+  //  int tmpYear = 2022;
+  //  day = tmpDay;
+  //  month = tmpMonth;
+  //  year = tmpYear;
+  day = now.Day();
+  month = now.Month();
+  year = now.Year();
+  if (day > 7) {
+    day -= 7;
+  } else {
+    switch (day) {
+      case 7:
+        day = 31;
+        break;
+      case 6:
+        day = 30;
+        break;
+      case 5:
+        day = 29;
+        break;
+      case 4:
+        day = 28;
+        break;
+      case 3:
+        day = 27;
+        break;
+      case 2:
+        day = 26;
+        break;
+      case 1:
+        day = 25;
+        break;
+      default:
+        day = 0;
+        break;
+    }
+    month -= 1;
+    if (month <= 0) {
+      month = 12;
+      year -= 1;
+    }
+  }
+  //  Serial.println("Delete Input : " + String(tmpDay) + "-" + String(tmpMonth) + "-" + String(tmpYear));
+  //  Serial.println("Delete Input : " + String(now.Day()) + "-" + String(now.Month()) + "-" + String(now.Year()));
+  Serial.println("Delete Output : " + String(day) + "-" + String(month) + "-" + String(year));
+  Firebase.RTDB.deleteNode(&fbdo, "/rainmeter/" + String(day) + "-" + String(month) + "-" + String(year));
+}
 
-  Note:
-  The function that starting the new TCP session i.e. first time server connection or previous session was closed, the function won't exit until the 
-  time of config.timeout.socketConnection.
+void t3Callback() {
+  Serial.print("water : ");
+  Serial.println(waterCount * amountOfWater);
+  Portal.handleClient();
+  //  if (WiFi.status() == WL_CONNECTED) {
+  //    Serial.println(printDateTime(now) + "Im connected...  :)");
+  //  } else {
+  //    Serial.println(printDateTime(now) + "Im not connected...  :(");
+  //  }
+  //  Serial.println(waterCount);}
+}
 
-  You can also set the TCP data sending retry with
-  config.tcp_data_sending_retry = 1;
+void t4Callback() {
+  counted = false;
+}
 
-  */
+void t5Callback() {
+
+}
+
+void t6Callback() {
+  if (previousWaterCount == waterCount) {
+    duplicateCount++;
+  } else {
+    duplicateCount = 0;
+  }
+  if (duplicateCount >= cooldownReset) {
+    waterCount = 0;
+  }
+  previousWaterCount = waterCount;
+}
+
+void t7Callback() {
+  sendLineNotify = true;
 }
 
 void loop()
 {
-  //Flash string (PROGMEM and FPSTR), Arduino String, C++ string, const char, char array, string literal are supported
-  //in all Firebase and FirebaseJson functions, unless F() macro is not supported.
-
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
-  {
-    sendDataPrevMillis = millis();
-
-    Serial.printf("Set bool... %s\n", Firebase.RTDB.setBool(&fbdo, "/test/bool", count % 2 == 0) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get bool... %s\n", Firebase.RTDB.getBool(&fbdo, "/test/bool") ? fbdo.to<bool>() ? "true" : "false" : fbdo.errorReason().c_str());
-
-    bool bVal;
-    Serial.printf("Get bool ref... %s\n", Firebase.RTDB.getBool(&fbdo, "/test/bool", &bVal) ? bVal ? "true" : "false" : fbdo.errorReason().c_str());
-
-    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/test/int", count) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get int... %s\n", Firebase.RTDB.getInt(&fbdo, "/test/int") ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
-//
-//    int iVal = 0;
-//    Serial.printf("Get int ref... %s\n", Firebase.RTDB.getInt(&fbdo, "/test/int", &iVal) ? String(iVal).c_str() : fbdo.errorReason().c_str());
-//
-//    Serial.printf("Set float... %s\n", Firebase.RTDB.setFloat(&fbdo, "/test/float", count + 10.2) ? "ok" : fbdo.errorReason().c_str());
-//
-//    Serial.printf("Get float... %s\n", Firebase.RTDB.getFloat(&fbdo, "/test/float") ? String(fbdo.to<float>()).c_str() : fbdo.errorReason().c_str());
-//
-//    Serial.printf("Set double... %s\n", Firebase.RTDB.setDouble(&fbdo, "/test/double", count + 35.517549723765) ? "ok" : fbdo.errorReason().c_str());
-//
-//    Serial.printf("Get double... %s\n", Firebase.RTDB.getDouble(&fbdo, "/test/double") ? String(fbdo.to<double>()).c_str() : fbdo.errorReason().c_str());
-//
-//    Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, "/test/string", "Hello World!") ? "ok" : fbdo.errorReason().c_str());
-//
-//    Serial.printf("Get string... %s\n", Firebase.RTDB.getString(&fbdo, "/test/string") ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
-
-    //For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create.ino
-    FirebaseJson json;
-
-    if (count == 0)
-    {
-      json.set("value/round/" + String(count), "cool!");
-      json.set("vaue/ts/.sv", "timestamp");
-      Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, "/test/json", &json) ? "ok" : fbdo.errorReason().c_str());
+  currentMillis = millis();
+  if ((currentMillis - previousMillis1 >= t1Interval)) {
+    Serial.println("T1 Run : ");
+    if (WiFi.status() == WL_CONNECTED && Firebase.ready() && previousMillis1 >= 5000) {
+      t1Callback();
     }
-    else
-    {
-      json.add(String(count), "smart!");
-      Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, "/test/json/value/round", &json) ? "ok" : fbdo.errorReason().c_str());
-    }
-
-    Serial.println();
-
-    //For generic set/get functions.
-
-    //For generic set, use Firebase.RTDB.set(&fbdo, <path>, <any variable or value>)
-
-    //For generic get, use Firebase.RTDB.get(&fbdo, <path>).
-    //And check its type with fbdo.dataType() or fbdo.dataTypeEnum() and
-    //cast the value from it e.g. fbdo.to<int>(), fbdo.to<std::string>().
-
-    //The function, fbdo.dataType() returns types String e.g. string, boolean,
-    //int, float, double, json, array, blob, file and null.
-
-    //The function, fbdo.dataTypeEnum() returns type enum (number) e.g. fb_esp_rtdb_data_type_null (1),
-    //fb_esp_rtdb_data_type_integer, fb_esp_rtdb_data_type_float, fb_esp_rtdb_data_type_double,
-    //fb_esp_rtdb_data_type_boolean, fb_esp_rtdb_data_type_string, fb_esp_rtdb_data_type_json,
-    //fb_esp_rtdb_data_type_array, fb_esp_rtdb_data_type_blob, and fb_esp_rtdb_data_type_file (10)
-
-    count++;
+    previousMillis1 = millis();
+  }
+  if (currentMillis - previousMillis2 >= t2Interval) {
+    Serial.println("T2 Run : ");
+    t2Callback();
+    previousMillis2 = millis();
+  }
+  if (currentMillis - previousMillis3 >= t3Interval) {
+    Serial.println("T3 Run : ");
+    t3Callback();
+    previousMillis3 = millis();
+  }
+  if (currentMillis - previousMillis4 >= t4Interval) {
+    Serial.println("T4 Run : ");
+    t4Callback();
+    previousMillis4 = millis();
+  }
+  if (currentMillis - previousMillis5 >= t5Interval) {
+    Serial.println("T5 Run : ");
+    t5Callback();
+    previousMillis5 = millis();
+  }
+  if (currentMillis - previousMillis6 >= t6Interval) {
+    Serial.println("T6 Run : ");
+    t6Callback();
+    previousMillis6 = millis();
+  }
+  if (currentMillis - previousMillis7 >= t7Interval) {
+    Serial.println("T7 Run : ");
+    t7Callback();
+    previousMillis7 = millis();
   }
 }
 
-/// PLEASE AVOID THIS ////
 
-//Please avoid the following inappropriate and inefficient use cases
-/**
- * 
- * 1. Call get repeatedly inside the loop without the appropriate timing for execution provided e.g. millis() or conditional checking,
- * where delay should be avoided.
- * 
- * Everytime get was called, the request header need to be sent to server which its size depends on the authentication method used, 
- * and costs your data usage.
- * 
- * Please use stream function instead for this use case.
- * 
- * 2. Using the single FirebaseData object to call different type functions as above example without the appropriate 
- * timing for execution provided in the loop i.e., repeatedly switching call between get and set functions.
- * 
- * In addition to costs the data usage, the delay will be involved as the session needs to be closed and opened too often
- * due to the HTTP method (GET, PUT, POST, PATCH and DELETE) was changed in the incoming request. 
- * 
- * 
- * Please reduce the use of swithing calls by store the multiple values to the JSON object and store it once on the database.
- * 
- * Or calling continuously "set" or "setAsync" functions without "get" called in between, and calling get continuously without set 
- * called in between.
- * 
- * If you needed to call arbitrary "get" and "set" based on condition or event, use another FirebaseData object to avoid the session 
- * closing and reopening.
- * 
- * 3. Use of delay or hidden delay or blocking operation to wait for hardware ready in the third party sensor libraries, together with stream functions e.g. Firebase.RTDB.readStream and fbdo.streamAvailable in the loop.
- * 
- * Please use non-blocking mode of sensor libraries (if available) or use millis instead of delay in your code.
- * 
- * 4. Blocking the token generation process.
- * 
- * Let the authentication token generation to run without blocking, the following code MUST BE AVOIDED.
- * 
- * while (!Firebase.ready()) <---- Don't do this in while loop
- * {
- *     delay(1000);
- * }
- * 
- */
+
+String printDateTime(const RtcDateTime & dt)
+{
+  snprintf_P(dateTimestring, countof(dateTimestring), PSTR("%02u-%02u-%04u %02u:%02u:%02u"), dt.Day(), dt.Month(), dt.Year(), dt.Hour(), dt.Minute(), dt.Second());
+  return dateTimestring;
+}
+
+String printDate(const RtcDateTime & dt)
+{
+  snprintf_P(datestring, countof(datestring), PSTR("%02u-%02u-%04u"), dt.Day(), dt.Month(), dt.Year());
+  return datestring;
+}
+
+void ICACHE_RAM_ATTR countWater() {
+  if (!counted) {
+    waterCount++;
+    counted = true;
+    Serial.print("water count : ");
+    Serial.println(waterCount);
+  }
+}
