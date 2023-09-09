348266b97fbc5399bd1bb417302fe97ab604f8c1
Jiraphat (Saifah) <jiraphat.t@idev.co.th>
2023-09-09 18:56:17 +0700

    09-09-2023

--- rain_meter_esp8266.ino
+++ rain_meter_esp8266.ino
@@ -1,19 +1,20 @@
-// Arduino IDE v1.8.16
+// Arduino IDE v1.8.16, v2.2.1
 // https://dl.espressif.com/dl/package_esp32_index.json
 // http://arduino.esp8266.com/stable/package_esp8266com_index.json
 // **Library**
-// ESP8266 v3.0.2
-// AutoConnect v1.3.1
-// PageBuilder v1.5.2
+// ESP8266 v3.0.2 , v3.1.2
+// AutoConnect v1.3.1 ,v1.4.2
+// PageBuilder v1.5.2, v1.5.6
 // Firebase Arduino Client Library for ESP8266 and ESP32 v3.3.0
 // Rtc By Makuna v2.3.5
 // DHT sensor library v1.4.3
-// ArduinoJson v6.18.5
-// Adafruit Unified Sense v1.1.5
+// ArduinoJson v6.18.5, v6.21.3
+// Adafruit Unified Senser v1.1.5
+// TridentTD_LineNotify v3.0.6
 // **Fix pyserial : https://forum.arduino.cc/t/pyserial-and-esptools-directory-error/671804/14
 
 // 2.794 millimeter per once
-// 90 miilmeter limit
+// 60 miilmeter limit
 // 20 min cooldwon reset
 void ICACHE_RAM_ATTR countWater();
 
@@ -61,6 +62,7 @@ DHT dht(DHTPIN, DHTTYPE);
 #define USER_EMAIL "cpe.rainmeter@gmail.com"
 #define USER_PASSWORD "cpe123456"
 
+// #include <TridentTD_LineNotify.h>
 #define LINE_TOKEN "f0E3HB6o7EubJvIVNw8AUC8QpoOeJn7H8sD3mUYm5K1"
 
 //  Rtc DS3231
@@ -80,7 +82,8 @@ FirebaseAuth auth;
 FirebaseConfig config;
 FirebaseJson json;
 
-unsigned long currentMillis, previousMillis1, previousMillis2, previousMillis3, previousMillis4, previousMillis5, previousMillis6, previousMillis7 = 0;
+// unsigned long currentMillis, previousMillis1, previousMillis2, previousMillis3, previousMillis4, previousMillis5, previousMillis6, previousMillis7, previousMillis8 = 0;
+unsigned long currentMillis, previousMillis1, previousMillis3, previousMillis4, previousMillis5, previousMillis6, previousMillis8 = 0;
 // Task 1 : Push data to Firebase (Database)
 // Task 2 : Delete Data 7 day ago
 // Task 3 : Handle WiFi
@@ -88,11 +91,13 @@ unsigned long currentMillis, previousMillis1, previousMillis2, previousMillis3, 
 // Task 5 : Read Temp, Hum And Push Data to Firebase
 // Task 6 : Reset cooldwon water
 // Task 7 : Debounce Line notify
-int t1Interval = pordMode ? 60000 * 15 : 15000;
-int t2Interval = pordMode ? 60000 * 60 : 60000 * 60;
-int t3Interval = 5000, t4Interval = 200, t5Interval = 2000;
+// Task 8 : Send line notify
+int t1Interval = pordMode ? 60000 * 10 : 30000;
+// int t2Interval = pordMode ? 60000 * 60 : 60000 * 60;
+int t3Interval = 6000, t4Interval = 200, t5Interval = 6000;
 int t6Interval = pordMode ? 60000 : 2000;
-int t7Interval = pordMode ? 60000 * 20 : 20000;
+// int t7Interval = pordMode ? 60000 * 20 : 20000;
+int t8Interval = pordMode ? 60000 * 15 : 30000;
 
 char dateTimestring[20];
 char datestring[11];
@@ -103,30 +108,31 @@ int waterCount = 0;
 float waterTotal = 0;
 bool counted = false;
 const float amountOfWater = 2.794;
-const int waterLimit = 90;
+int waterLimit = 60;
 
 const int cooldownReset = 20;
 int previousWaterCount = 0;
 int duplicateCount = 0;
-boolean sendLineNotify = true;
+// boolean sendLineNotify = true;
 
 int day = 0;
 int month = 0;
 int year = 0;
 
-//File indexFile;
-//File dataFile;
-File myFile;
+// File myFile;
 const int sdCardPin = D4;
 
 int h = 0;
 int t = 0;
 
-WiFiClientSecure client;
 String req;
 // ข้อความ ที่จะแสดงใน Line
-String txt1 = "ปริมาณน้ำฝนเกิน 90 มิลลิเมตรแล้ว !";
-String line;
+String txt1 = "ปริมาณน้ำฝนเกิน 60 มิลลิเมตรแล้ว !";
+String txt2 = "ปริมาณน้ำฝนเกิน 90 มิลลิเมตรแล้ว !";
+// String line;
+
+String settingTime;
+String settingWater;
 
 void setup() {
   //  ปี เดือน วัน ชั่วโมง นาที วินาที
@@ -207,6 +213,12 @@ void setup() {
   Firebase.reconnectWiFi(false);
   Firebase.setDoubleDigits(5);
 
+  settingTime = Firebase.RTDB.getString(&fbdo, F("/settingTime")) ? fbdo.to<const char*>() : fbdo.errorReason().c_str();
+  settingWater = Firebase.RTDB.getString(&fbdo, F("/settingWater")) ? fbdo.to<const char*>() : fbdo.errorReason().c_str();
+
+  // Serial.println(settingTime);
+  // Serial.println(settingWater);
+
   req = "POST /api/notify HTTP/1.1\r\n";
   req += "Host: notify-api.line.me\r\n";
   req += "Authorization: Bearer " + String(LINE_TOKEN) + "\r\n";
@@ -216,200 +228,109 @@ void setup() {
   req += "Content-Type: application/x-www-form-urlencoded\r\n";
   req += "Content-Length: " + String(String("message=" + txt1).length()) + "\r\n";
   req += "\r\n";
-  req += "message=" + txt1;
-}
 
-void t1Callback() {
-  waterTotal = waterCount * amountOfWater;
-  if (waterTotal >= waterLimit) {
-    if (sendLineNotify) {
-      notifyLine();
-      sendLineNotify = false;
-    }
-  }
-
-  now = Rtc.GetDateTime();
-  dateTimeNow = printDateTime(now);
-  json.set("water", waterTotal);
-  json.set("time", dateTimeNow);
-  //  json.set("time/.sv", "timestamp");
-  //  Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, "/testset", &json) ? "ok" : fbdo.errorReason().c_str());
-  //  Firebase.RTDB.set(&fbdo, "/testset", &json);
-  //  Firebase.RTDB.set(&fbdo, "/rainmeter/" + printDate(now) + "/" + dateTimeNow, &json);
-  //  Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, "/rainmeter/" + printDate(now) + "/" + dateTimeNow, &json) ? "ok" : fbdo.errorReason().c_str());
-
-  Firebase.RTDB.pushJSON(&fbdo, F("/rainmeter/") + printDate(now), &json);
-  Serial.println("Push data to firebase");
-  //  Serial.println(Firebase.RTDB.pushJSON(&fbdo, "/rainmeter/" + printDate(now), &json));
-
-  myFile = SD.open(printDate(now) + ".txt", FILE_WRITE);
-  // ถ้าเปิดไฟล์สำเร็จ ให้เขียนข้อมูลเพิ่มลงไป
-  if (myFile) {
-    //    Serial.print("Writing to test.txt...");
-    myFile.println("time:" + dateTimeNow + ",water:" + waterTotal + ",temp:" + t + ",hum:" + h);  // สั่งให้เขียนข้อมูล
-    myFile.close();                                                                               // ปิดไฟล์
-    //    Serial.println("done.");
+  if (settingWater == "90") {
+    req += "message=" + txt1;
   } else {
-    // ถ้าเปิดไฟลืไม่สำเร็จ ให้แสดง error
-    Serial.println("error opening " + printDate(now) + ".txt");
+    req += "message=" + txt2;
   }
-  //  notifyLine();
-}
 
-void t2Callback() {
-  now = Rtc.GetDateTime();
-  //  int tmpDay = 1;
-  //  int tmpMonth = 1;
-  //  int tmpYear = 2022;
-  //  day = tmpDay;
-  //  month = tmpMonth;
-  //  year = tmpYear;
-  day = now.Day();
-  month = now.Month();
-  year = now.Year();
-  if (day > 7) {
-    day -= 7;
+  if (settingTime == "10") {
+    t1Interval = pordMode ? 60000 * 10 : 30000;
   } else {
-    switch (day) {
-      case 7:
-        day = 31;
-        break;
-      case 6:
-        day = 30;
-        break;
-      case 5:
-        day = 29;
-        break;
-      case 4:
-        day = 28;
-        break;
-      case 3:
-        day = 27;
-        break;
-      case 2:
-        day = 26;
-        break;
-      case 1:
-        day = 25;
-        break;
-      default:
-        day = 0;
-        break;
-    }
-    month -= 1;
-    if (month <= 0) {
-      month = 12;
-      year -= 1;
-    }
+    t1Interval = pordMode ? 60000 * 15 : 30000;
   }
-  //  Serial.println("Delete Input : " + String(tmpDay) + "-" + String(tmpMonth) + "-" + String(tmpYear));
-  //  Serial.println("Delete Input : " + String(now.Day()) + "-" + String(now.Month()) + "-" + String(now.Year()));
-  Serial.println("Delete Output : " + String(day) + "-" + String(month) + "-" + String(year));
-  Firebase.RTDB.deleteNode(&fbdo, "/rainmeter/" + String(day) + "-" + String(month) + "-" + String(year));
-}
-
-void t3Callback() {
-  Serial.print(F("water : "));
-  Serial.println(waterTotal);
-  Portal.handleClient();
-}
-
-void t4Callback() {
-  counted = false;
-}
-
-void t5Callback() {
-  h = dht.readHumidity();
-  t = dht.readTemperature();
-  if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    h = 0;
-    t = 0;
-  }
-  //  Serial.print(F("Humidity: "));
-  //  Serial.print(h);
-  //  Serial.print(F(" %  Temperature: "));
-  //  Serial.print(t);
-  //  Serial.println(F(" C "));
-
-  Firebase.RTDB.set(&fbdo, F("/test/temp"), t);
-  Firebase.RTDB.set(&fbdo, F("/test/hum"), h);
-}
-
-void t6Callback() {
-  if (previousWaterCount == waterCount) {
-    duplicateCount++;
-  } else {
-    duplicateCount = 0;
-  }
-  if (duplicateCount >= cooldownReset) {
-    waterCount = 0;
-  }
-  previousWaterCount = waterCount;
-}
-
-void t7Callback() {
-  sendLineNotify = true;
 }
 
 void loop() {
   currentMillis = millis();
   if (currentMillis - previousMillis1 >= t1Interval) {
-    if (WiFi.status() == WL_CONNECTED && Firebase.ready() && previousMillis1 >= 10000) {
+    // if (WiFi.status() == WL_CONNECTED && Firebase.ready() && previousMillis1 >= 10000) {
+    if (Firebase.ready() && previousMillis1 >= 10000) {
       Serial.println(F("T1 Run : "));
-      t1Callback();
+      waterTotal = waterCount * amountOfWater;
+      now = Rtc.GetDateTime();
+      dateTimeNow = printDateTime(now);
+      json.set("water", waterTotal);
+      json.set("time", dateTimeNow);
+
+      Firebase.RTDB.pushJSON(&fbdo, F("/rainmeter/") + printDate(now), &json);
+      Serial.println("Push data to firebase");
     }
     previousMillis1 = millis();
   }
-  if (currentMillis - previousMillis2 >= t2Interval) {
-    Serial.println(F("T2 Run : "));
-    t2Callback();
-    previousMillis2 = millis();
-  }
   if (currentMillis - previousMillis3 >= t3Interval) {
-    //    Serial.println("T3 Run : ");
-    t3Callback();
+    // Serial.println("T3 Run : ");
+    // t3Callback();
+    Serial.print(F("water : "));
+    Serial.println(waterTotal);
+    Portal.handleClient();
     previousMillis3 = millis();
   }
   if (currentMillis - previousMillis4 >= t4Interval) {
-    //    Serial.println("T4 Run : ");
-    t4Callback();
+    // Serial.println("T4 Run : ");
+    // t4Callback();
+    counted = false;
     previousMillis4 = millis();
   }
   if (currentMillis - previousMillis5 >= t5Interval) {
-    //    Serial.println("T5 Run : ");
-    t5Callback();
+    // Serial.println("T5 Run : ");
+    // t5Callback();
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    if (isnan(h) || isnan(t)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      h = 0;
+      t = 0;
+    }
+    Firebase.RTDB.set(&fbdo, F("/test/temp"), t);
+    Firebase.RTDB.set(&fbdo, F("/test/hum"), h);
     previousMillis5 = millis();
   }
   if (currentMillis - previousMillis6 >= t6Interval) {
-    //    Serial.println("T6 Run : ");
-    t6Callback();
+    // Serial.println("T6 Run : ");
+    // t6Callback();
+    if (previousWaterCount == waterCount) {
+      duplicateCount++;
+    } else {
+      duplicateCount = 0;
+    }
+    if (duplicateCount >= cooldownReset) {
+      waterCount = 0;
+    }
+    previousWaterCount = waterCount;
     previousMillis6 = millis();
   }
-  if (currentMillis - previousMillis7 >= t7Interval) {
-    //    Serial.println("T7 Run : ");
-    t7Callback();
-    previousMillis7 = millis();
+  // if (currentMillis - previousMillis7 >= t7Interval) {
+  // Serial.println("T7 Run : ");
+  // t7Callback();
+  // previousMillis7 = millis();
+  // }
+  if (currentMillis - previousMillis8 >= t8Interval && waterTotal >= waterLimit) {
+    // Serial.println("T8 Run : ");
+    notifyLine();
+    previousMillis8 = millis();
   }
 }
 
 void notifyLine() {
+  WiFiClientSecure client;
   client.setInsecure();
   if (!client.connect("notify-api.line.me", 443)) {
-    Serial.println("ERROR Connection failed");
+    Serial.println("ERROR Connection notify-api.line.me failed");
     client.stop();
     return;
   }
   client.print(req);
-  Serial.println(F("[Response:]"));
-  while (client.connected()) {
-    if (client.available()) {
-      line = client.readStringUntil('\n');
-      Serial.println(line);
-    }
-  }
+  // Serial.println(F("[Response:]"));
+  // while (client.connected()) {
+  //   if (client.available()) {
+  //     line = client.readStringUntil('\n');
+  //     Serial.println(line);
+  //   }
+  // }
   client.stop();
-  Serial.println(F("\n[Disconnected]"));
+  // Serial.println(F("\n[Disconnected]"));
 }
 
 String printDateTime(const RtcDateTime& dt) {
@@ -427,7 +348,5 @@ void ICACHE_RAM_ATTR countWater() {
     waterCount++;
     waterTotal = waterCount * amountOfWater;
     counted = true;
-    //    Serial.print("water count : ");
-    //    Serial.println(waterCount);
   }
 }
